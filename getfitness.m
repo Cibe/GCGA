4ed7220c6b7694054ca585c51f5fcce7fb4e5f8b
Cibe <k.cibehariharan@gmail.com>
2014-03-23 12:30:22 +0530

    refactored

--- getfitness.m
+++ getfitness.m
@@ -1,8 +1,8 @@
-function [ fitnessVal ] = getfitness( adjMat,chromosom )
+function [ fitnessVal ] = getfitness( adjMat,chromosome )
 fitnessVal=0;
 for i=1:size(adjMat,1)
   for j=1:size(adjMat,2)
-      if adjMat(i,j)==1 && chromosom(i)==chromosom(j) 
+      if adjMat(i,j)==1 && chromosome(i)==chromosome(j) 
         fitnessVal=fitnessVal+1;
       end
   end
